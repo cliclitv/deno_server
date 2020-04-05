f3258b389f1d2adcf3b04d6c877d26f848f52055
132yse <1533540012@qq.com>
2020-04-05 18:32:16 +0800

    deno fmt

--- src/cloud_group.ts
+++ src/cloud_group.ts
@@ -1,70 +1,82 @@
-import { Context } from 'server/mod.ts'
-import { getToken } from './util.ts'
+import { Context } from "server/mod.ts";
+import { getToken } from "./util.ts";
 
 export default async (c: Context) => {
-  const [content, tag] = c.queryParams.url.split('@')
-  let url = content
-  let type = content.includes('m3u8') ? 'hls' : 'mp4'
+  const [content, tag] = c.queryParams.url.split("@");
+  let url = content;
+  let type = content.includes("m3u8") ? "hls" : "mp4";
   switch (tag) {
-    case 'hcy':
-      const info = atob(content).split(',')
-      url = `https://caiyun.feixin.10086.cn/webdisk2/downLoadAction!downloadToPC.action?contentID=${info[1]}&shareContentIDs=${info[1]}&catalogList=&downloadSize=214446914`
-      const cookie: string = await fetch(`https://api.clicli.us/cookie/${info[0]}`)
+    case "hcy":
+      const info = atob(content).split(",");
+      url =
+        `https://caiyun.feixin.10086.cn/webdisk2/downLoadAction!downloadToPC.action?contentID=${info[1]}&shareContentIDs=${info[1]}&catalogList=&downloadSize=214446914`;
+      const cookie: string = await fetch(
+        `https://api.clicli.us/cookie/${info[0]}`,
+      )
         .then((resp) => resp.json())
-        .then((data) => data.result.hcy)
+        .then((data) => data.result.hcy);
       url = await fetch(url, {
         headers: {
           Cookie: cookie,
-          Host: 'caiyun.feixin.10086.cn',
+          Host: "caiyun.feixin.10086.cn",
         },
       })
         .then((resp) => resp.json())
-        .then((data) => data.downloadUrl)
-      return { url, type: 'mp4' }
-    case 'dogecloud':
-      const ip = (c.request.conn.remoteAddr as Deno.NetAddr).hostname
-      const param = `/video/streams.json?platform=pch5&vid=${content}&ip=${ip}`
-      const token = getToken(param + '\n')
+        .then((data) => data.downloadUrl);
+      return { url, type: "mp4" };
+    case "dogecloud":
+      const ip = (c.request.conn.remoteAddr as Deno.NetAddr).hostname;
+      const param = `/video/streams.json?platform=pch5&vid=${content}&ip=${ip}`;
+      const token = getToken(param + "\n");
       url = await fetch(`https://api.dogecloud.com${param}`, {
         headers: {
-          Host: 'api.dogecloud.com',
+          Host: "api.dogecloud.com",
           Authorization: `TOKEN ${token}`,
         },
       })
         .then((resp) => resp.json())
-        .then((data) => data.data.stream[0].url)
-      return { url, type: 'hls' }
-    case '1096':
-      const vid = await fetch(`https://www.wegame.com.cn/api/forum/lua/wegame_feeds/get_feed_item_data?p={"iid":"${content}","uid":211762212}`)
+        .then((data) => data.data.stream[0].url);
+      return { url, type: "hls" };
+    case "1096":
+      const vid = await fetch(
+        `https://www.wegame.com.cn/api/forum/lua/wegame_feeds/get_feed_item_data?p={"iid":"${content}","uid":211762212}`,
+      )
         .then((resp) => resp.json())
-        .then((data) => JSON.parse(data.data.data.data).video.vid)
+        .then((data) => JSON.parse(data.data.data.data).video.vid);
 
-      url = await fetch(`https://qt.qq.com/php_cgi/cod_video/php/get_video_url.php?json=1&multirate=1&filetype=40&game_id=123456&vid=${vid}`)
+      url = await fetch(
+        `https://qt.qq.com/php_cgi/cod_video/php/get_video_url.php?json=1&multirate=1&filetype=40&game_id=123456&vid=${vid}`,
+      )
         .then((resp) => resp.json())
-        .then((data) => data.data[0].data[0])
+        .then((data) => data.data[0].data[0]);
 
-      return { url, type: 'mp4' }
-    case '1072':
-      url = await fetch(`https://api.pengyou.com/go-cgi-bin/moment_h5/getFeedDetail?feedId=${content}`)
+      return { url, type: "mp4" };
+    case "1072":
+      url = await fetch(
+        `https://api.pengyou.com/go-cgi-bin/moment_h5/getFeedDetail?feedId=${content}`,
+      )
         .then((resp) => resp.json())
-        .then((data) => data.result.contents[0].video.urls.f0)
-      return { url, type: 'mp4' }
-    case 'weibo':
+        .then((data) => data.result.contents[0].video.urls.f0);
+      return { url, type: "mp4" };
+    case "weibo":
       url = await fetch(`https://m.weibo.cn/statuses/show?id=${content}`)
         .then((resp) => resp.json())
-        .then((data) => data.data.page_info.urls.mp4_720p_mp4.replace('http', 'https'))
+        .then((data) =>
+          data.data.page_info.urls.mp4_720p_mp4.replace("http", "https")
+        );
 
-      return { url, type: 'mp4' }
+      return { url, type: "mp4" };
     default:
       // 时光的处理
-      if (content.includes('1098')) {
+      if (content.includes("1098")) {
         const res: string = await fetch(content)
           .then((resp) => resp.url)
-          .then((data) => data)
-        const sha = res.replace(/(\S*)1098/, '')
-        url = `https://apd-vliveachy.apdcdn.tc.qq.com/vmtt.tc.qq.com/1098` + sha
-        return { url, type: 'mp4' }
+          .then((data) => data);
+        const sha = res.replace(/(\S*)1098/, "");
+        url = `https://apd-vliveachy.apdcdn.tc.qq.com/vmtt.tc.qq.com/1098` +
+          sha;
+        return { url, type: "mp4" };
       }
-      return { url, type }
+      return { url, type };
   }
-}
+};
