3fa0731cc1f8ae5d6baf4f92e6ba7cc978d2328d
132yse <1533540012@qq.com>
2020-04-05 14:56:48 +0800

    增加demon

--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
 // import "dotenv/load.ts";
-import { Application } from "server/mod.ts";
-import { logger } from "mw/logger.ts";
-import { cors } from "mw/cors.ts";
-import cloudGroup from "./src/cloud_group.ts";
+import { Application } from 'server/mod.ts'
+import { logger } from 'mw/logger.ts'
+import { cors } from 'mw/cors.ts'
+import cloudGroup from './src/cloud_group.ts'
 // import pvGroup from "./src/pv_group.ts";
-import uploadGroup from "./src/upload_group.ts";
+import uploadGroup from './src/upload_group.ts'
 
-const app = new Application();
+const app = new Application()
 
-app.use(logger()).use(cors());
+app.use(logger()).use(cors())
 
-app.start({ hostname: "0.0.0.0", port: 8083 });
+app.start({ hostname: '0.0.0.0', port: 8083 })
 
-console.log(
-  `server listening on http://localhost:${(app.server?.listener
-    .addr as Deno.NetAddr).port}`,
-);
+console.log(`server listening on http://localhost:${(app.server?.listener.addr as Deno.NetAddr).port}`)
 
-cloudGroup(app.group("/cloud"));
+app.get('/jx', cloudGroup)
 // pv: page views
 // pvGroup(app.group("/pv"));
-uploadGroup(app.group("/upload"));
+uploadGroup(app.group('/upload'))
