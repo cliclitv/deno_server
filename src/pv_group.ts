8dfb79af1a0f8f3daf45836a20c3ef34dd24dd2c
木杉 <zhmushan@qq.com>
2020-04-06 21:31:56 +0800

    disable log

--- src/pv_group.ts
+++ src/pv_group.ts
@@ -4,22 +4,17 @@ import { client } from "./db.ts";
 export default function (g: Group) {
   g.get("/:pid", async (c) => {
     const pid = parseInt(c.params.pid);
-    const pv = await client.transaction(async (conn) => {
-      const pvs = (await conn.query(`select pv from pv where pid = ?`, [
-        pid,
-      ])) as Array<{ pv: number }>;
-      if (pvs.length) {
-        const pv = pvs[0].pv + 1;
-        await conn.execute(`update pv set pv = ? where pid = ?`, [pv, pid]);
-        return pv;
-      } else {
-        await conn.execute(`insert into pv(pv, pid) values(?, ?)`, [1, pid]);
-      }
-
-      conn.close();
-      return 1;
-    });
-
-    return { pid, pv };
+    return client
+      .query(`select pv from pv where pid = ?`, [pid])
+      .then((pvs) => {
+        let pv = 1;
+        if (pvs.length) {
+          pv = pvs[0].pv + 1;
+          client.execute(`update pv set pv = ? where pid = ?`, [pv, pid]);
+        } else {
+          client.execute(`insert into pv(pv, pid) values(?, ?)`, [pv, pid]);
+        }
+        return { pid, pv };
+      });
   });
 }
